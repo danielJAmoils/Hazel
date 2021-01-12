6cefd0fb7e053524ddb89efb531a6845a8df6275
Daniel Amoils <danieljamoils@gmail.com>
2021-01-11 19:32:13 -0500

    Shader Library

--- Hazel/src/Platform/OpenGL/OpenGLShader.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLShader.cpp
@@ -6,8 +6,7 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
-namespace Hazel
-{
+namespace Hazel {
 
 	static GLenum ShaderTypeFromString(const std::string &type)
 	{
@@ -25,9 +24,17 @@ namespace Hazel
 		std::string source = ReadFile(filepath);
 		auto shaderSources = PreProcess(source);
 		Compile(shaderSources);
+
+		//Extract name from filepath
+		auto lastSlash = filepath.find_last_of("/\\");
+		lastSlash = lastSlash == std::string::npos ? 0 : lastSlash + 1;
+		auto lastDot = filepath.rfind('.');
+		auto count = lastDot == std::string::npos ? filepath.size() - lastSlash : lastDot - lastSlash;
+		m_Name = filepath.substr(lastSlash, count);
 	}
 
-	OpenGLShader::OpenGLShader(const std::string &vertexSrc, const std::string &fragmentSrc)
+	OpenGLShader::OpenGLShader(const std::string& name, const std::string &vertexSrc, const std::string &fragmentSrc)
+		: m_Name(name)
 	{
 		std::unordered_map<GLenum, std::string> sources;
 		sources[GL_VERTEX_SHADER] = vertexSrc;
@@ -43,7 +50,7 @@ namespace Hazel
 	std::string OpenGLShader::ReadFile(const std::string &filepath)
 	{
 		std::string result;
-		std::ifstream in(filepath, std::ios::in, std::ios::binary);
+		std::ifstream in(filepath, std::ios::in | std::ios::binary);
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
@@ -86,7 +93,9 @@ namespace Hazel
 	void OpenGLShader::Compile(const std::unordered_map<GLenum, std::string> &shaderSources)
 	{
 		GLuint program = glCreateProgram();
-		std::vector<GLenum> glShaderIDs(shaderSources.size());
+		HZ_CORE_ASSERT(shaderSources.size() <= 2, "We only support 2 shaders for now");
+		std::array<GLenum, 2> glShaderIDs;
+		int glShaderIDIndex = 0;
 		for (auto &kv : shaderSources)
 		{
 			GLenum type = kv.first;
@@ -117,7 +126,7 @@ namespace Hazel
 			}
 
 			glAttachShader(program, shader);
-			glShaderIDs.push_back(shader);
+			glShaderIDs[glShaderIDIndex++] = shader;
 		}
 
 		m_RendererID = program;
@@ -206,4 +215,4 @@ namespace Hazel
 		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
 	}
 
-} // namespace Hazel
+}
