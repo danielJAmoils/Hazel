ff796e9e0e8c0d637cf3269e789b35510f48b0a0
Daniel Amoils <danieljamoils@gmail.com>
2021-02-06 20:48:26 -0500

    Add timer for profiling

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -16,13 +16,11 @@ namespace Hazel {
 
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
-		virtual void OnImGuiRender() override;
 
 		void Begin();
 		void End();
 	private:
 		float m_Time = 0.0f;
-
 	};
 
 }
