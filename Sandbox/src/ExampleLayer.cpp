e66b1aaf08e704288b1b137d20264db0845d7536
Daniel Amoils <danieljamoils@gmail.com>
2021-02-17 22:03:01 -0500

    Add wrapers to LayerStack and fix formating in example layer

--- Sandbox/src/ExampleLayer.cpp
+++ Sandbox/src/ExampleLayer.cpp
@@ -53,10 +53,13 @@ ExampleLayer::ExampleLayer()
 			
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
+
 			uniform mat4 u_ViewProjection;
 			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
 			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
@@ -69,8 +72,10 @@ ExampleLayer::ExampleLayer()
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
 			in vec4 v_Color;
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
@@ -84,9 +89,12 @@ ExampleLayer::ExampleLayer()
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+
 			uniform mat4 u_ViewProjection;
 			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
+
 			void main()
 			{
 				v_Position = a_Position;
@@ -98,9 +106,11 @@ ExampleLayer::ExampleLayer()
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
 			
 			uniform vec3 u_Color;
+
 			void main()
 			{
 				color = vec4(u_Color, 1.0);
